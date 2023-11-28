381859dd511cd5452d17d704a2bd1ad1884aac57
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2023-11-28 03:50:34 -0800

    add task runner

--- sort.go
+++ sort.go
@@ -2,11 +2,18 @@ package topological
 
 import (
 	"cmp"
+	"context"
 	"fmt"
 	"slices"
+	"sync"
 )
 
-func Sort[T any, ID cmp.Ordered](elements []T, elementID func(T) ID, elementEdges func(T) []ID) ([]T, error) {
+type (
+	EdgeFunc[T any, ID cmp.Ordered]       func(T) []ID
+	IdentifierFunc[T any, ID cmp.Ordered] func(T) ID
+)
+
+func Sort[T any, ID cmp.Ordered](elements []T, elementID IdentifierFunc[T, ID], elementEdges EdgeFunc[T, ID]) error {
 	var (
 		visited   = make([]bool, 2*len(elements))
 		temporal  = visited[:len(elements)]
@@ -42,8 +49,115 @@ func Sort[T any, ID cmp.Ordered](elements []T, elementID func(T) ID, elementEdge
 	}
 	for _, e := range elements {
 		if err := visit(elementID(e)); err != nil {
-			return nil, err
+			return err
 		}
 	}
-	return sorted, nil
+	copy(elements, sorted)
+	return nil
+}
+
+type Result[V any] struct {
+	Value V
+	Err   error
+}
+
+type TaskFunc[T, V any] func(T, context.Context, []V) (V, error)
+
+type result[ID cmp.Ordered, V any] struct {
+	ID    ID
+	Value V
+	Error error
+}
+
+func Tasks[ID cmp.Ordered, T, V any](ctx context.Context, elements []T, elementID IdentifierFunc[T, ID], elementEdges EdgeFunc[T, ID], elementTask TaskFunc[T, V]) error {
+	err := Sort[T, ID](elements, elementID, elementEdges)
+	if err != nil {
+		return err
+	}
+	results := make([]V, len(elements))
+	done := make([]ID, 0, len(elements))
+
+	consumeResult := func(r result[ID, V]) error {
+		if r.Error != nil {
+			return r.Error
+		}
+		for i := range elements {
+			if elementID(elements[i]) == r.ID {
+				done = append(done, r.ID)
+				results[i] = r.Value
+				break
+			}
+		}
+		return nil
+	}
+	inputs := func(index int, in []ID) []V {
+		params := make([]V, len(in))
+		for i, id := range in {
+			for j := range elements[:index] {
+				if elementID(elements[j]) == id {
+					params[i] = results[j]
+					break
+				}
+			}
+		}
+		return params
+	}
+
+	c := make(chan result[ID, V])
+
+	wg := sync.WaitGroup{}
+	var cleanup sync.Once
+
+	next := 0
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case r, ok := <-c:
+			if !ok {
+				break loop
+			}
+			if err := consumeResult(r); err != nil {
+				return err
+			}
+		default:
+			if next >= len(elements) {
+				cleanup.Do(func() {
+					go func() {
+						wg.Wait()
+						close(c)
+					}()
+				})
+				continue
+			}
+			edges := elementEdges(elements[next])
+			if !isSubset(done, edges) {
+				continue
+			}
+			el := elements[next]
+			id := elementID(el)
+			wg.Add(1)
+			go func(id ID, element T, in []V) {
+				defer wg.Done()
+				res, err := elementTask(el, ctx, in)
+				c <- result[ID, V]{
+					ID:    id,
+					Value: res,
+					Error: err,
+				}
+			}(id, el, inputs(next, edges))
+			next++
+		}
+	}
+	return nil
+}
+
+func isSubset[T cmp.Ordered](set, subset []T) bool {
+	for _, v := range subset {
+		if !slices.Contains(set, v) {
+			return false
+		}
+	}
+	return true
 }
--- sort_test.go
+++ sort_test.go
@@ -1,9 +1,39 @@
-package topological
+package topological_test
 
 import (
+	"cmp"
+	"context"
+	"errors"
+	"fmt"
 	"slices"
 	"testing"
+	"time"
+
+	"github.com/crhntr/topological"
 )
+
+type Recipe struct {
+	ID          int
+	Ingredients []int
+	CookTime    time.Duration
+	IsBad       bool
+}
+
+func identifiers[T any, ID cmp.Ordered](in []T, id func(T) ID) []ID {
+	ids := make([]ID, len(in))
+	for i := range in {
+		ids[i] = id(in[i])
+	}
+	return ids
+}
+
+func (p Recipe) Edges() []int {
+	return slices.Clone(p.Ingredients)
+}
+
+func (p Recipe) Identifier() int {
+	return p.ID
+}
 
 func TestSort(t *testing.T) {
 	t.Run("no dependencies", func(t *testing.T) {
@@ -13,15 +43,12 @@ func TestSort(t *testing.T) {
 				{ID: 2},
 				{ID: 3},
 			}
-			sorted, err := Sort(recipes, func(r Recipe) int { return r.ID }, func(r Recipe) []int { return r.Edges() })
+			err := topological.Sort(recipes, Recipe.Identifier, Recipe.Edges)
 			if err != nil {
 				t.Fatal(err)
 			}
-			if len(sorted) != len(recipes) {
-				t.Fatalf("expected %d recipes, got %d", len(recipes), len(sorted))
-			}
-			if exp := []int{1, 2, 3}; !slices.Equal(identifiers(sorted), exp) {
-				t.Errorf("expected recipes to be sorted like %v, got %v", exp, identifiers(sorted))
+			if exp, got := []int{1, 2, 3}, identifiers(recipes, Recipe.Identifier); !slices.Equal(got, exp) {
+				t.Errorf("expected recipes to be sorted like %v, got %v", exp, got)
 			}
 		})
 		t.Run("decreasing", func(t *testing.T) {
@@ -30,12 +57,12 @@ func TestSort(t *testing.T) {
 				{ID: 2},
 				{ID: 1},
 			}
-			sorted, err := Sort(recipes, func(r Recipe) int { return r.ID }, func(r Recipe) []int { return r.Edges() })
+			err := topological.Sort(recipes, Recipe.Identifier, Recipe.Edges)
 			if err != nil {
 				t.Fatal(err)
 			}
-			if exp := []int{1, 2, 3}; !slices.Equal(identifiers(sorted), exp) {
-				t.Errorf("expected recipes to be sorted like %v, got %v", exp, identifiers(sorted))
+			if exp, got := []int{1, 2, 3}, identifiers(recipes, Recipe.Identifier); !slices.Equal(got, exp) {
+				t.Errorf("expected recipes to be sorted like %v, got %v", exp, got)
 			}
 		})
 	})
@@ -49,21 +76,21 @@ func TestSort(t *testing.T) {
 				{ID: 4, Ingredients: []int{}},
 				{ID: 5, Ingredients: []int{}},
 			}
-			sorted, err := Sort(recipes, func(r Recipe) int { return r.ID }, func(r Recipe) []int { return r.Edges() })
+			err := topological.Sort(recipes, Recipe.Identifier, Recipe.Edges)
 			if err != nil {
 				t.Fatal(err)
 			}
-			if exp := []int{4, 5, 2, 3, 1}; !slices.Equal(identifiers(sorted), exp) {
-				t.Errorf("expected recipes to be sorted like %v, got %v", exp, identifiers(sorted))
+			if exp, got := []int{4, 5, 2, 3, 1}, identifiers(recipes, Recipe.Identifier); !slices.Equal(got, exp) {
+				t.Errorf("expected recipes to be sorted like %v, got %v", exp, got)
 			}
 		})
 		t.Run("self reference", func(t *testing.T) {
 			recipes := []Recipe{
 				{ID: 1, Ingredients: []int{1}},
 			}
-			_, err := Sort(recipes, func(r Recipe) int { return r.ID }, func(r Recipe) []int { return r.Edges() })
+			err := topological.Sort(recipes, Recipe.Identifier, Recipe.Edges)
 			if err == nil {
-				t.Error(err)
+				t.Error("expected an error")
 			}
 		})
 		t.Run("loop", func(t *testing.T) {
@@ -72,31 +99,117 @@ func TestSort(t *testing.T) {
 				{ID: 2, Ingredients: []int{3}},
 				{ID: 3, Ingredients: []int{1}},
 			}
-			_, err := Sort(recipes, func(r Recipe) int { return r.ID }, func(r Recipe) []int { return r.Edges() })
+			err := topological.Sort(recipes, Recipe.Identifier, Recipe.Edges)
 			if err == nil {
-				t.Error(err)
+				t.Error("expected an error")
 			}
 		})
 	})
 }
 
-func identifiers(recipes []Recipe) []int {
-	ids := make([]int, len(recipes))
-	for i, r := range recipes {
-		ids[i] = r.ID
+type Ingredient struct {
+	RecipeID int
+	Done     bool
+}
+
+func (i Ingredient) RecipeIdentifier() int {
+	return i.RecipeID
+}
+
+var _ topological.TaskFunc[Recipe, Ingredient] = Recipe.Cook
+
+var ErrBadRecipe = errors.New("bad recipe")
+
+func (p Recipe) Cook(ctx context.Context, in []Ingredient) (Ingredient, error) {
+	if p.IsBad {
+		return Ingredient{}, ErrBadRecipe
 	}
-	return ids
+	for i, requirement := range p.Ingredients {
+		if i >= len(in) || in[i].RecipeID != requirement {
+			return Ingredient{}, fmt.Errorf("missing requirement %d", requirement)
+		}
+	}
+	select {
+	case <-ctx.Done():
+		return Ingredient{RecipeID: p.ID}, ctx.Err()
+	case <-time.After(p.CookTime):
+		return Ingredient{RecipeID: p.ID, Done: true}, nil
+	}
 }
 
-type Recipe struct {
-	ID          int
-	Ingredients []int
-}
-
-func (p Recipe) Edges() []int {
-	return slices.Clone(p.Ingredients)
-}
-
-func (p Recipe) Identifier() int {
-	return p.ID
+func TestTasks(t *testing.T) {
+	t.Run("no dependencies", func(t *testing.T) {
+		ctx := context.Background()
+		s := time.Second / 6
+		recipes := []Recipe{
+			{ID: 1, CookTime: s},
+			{ID: 2, CookTime: s},
+			{ID: 3, CookTime: s},
+			{ID: 4, CookTime: s},
+			{ID: 5, CookTime: s},
+			{ID: 6, CookTime: s},
+		}
+		ctx, cancel := context.WithTimeout(ctx, s*time.Duration(len(recipes))/2)
+		t.Cleanup(cancel)
+		err := topological.Tasks(ctx, recipes, Recipe.Identifier, Recipe.Edges, Recipe.Cook)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+	t.Run("self reference", func(t *testing.T) {
+		recipes := []Recipe{
+			{ID: 1, Ingredients: []int{1}},
+		}
+		ctx := context.Background()
+		err := topological.Tasks(ctx, recipes, Recipe.Identifier, Recipe.Edges, Recipe.Cook)
+		if err == nil {
+			t.Error("expected and error")
+		}
+	})
+	t.Run("one task", func(t *testing.T) {
+		recipes := []Recipe{
+			{ID: 1, CookTime: time.Second / 20},
+		}
+		ctx := context.Background()
+		err := topological.Tasks(ctx, recipes, Recipe.Identifier, Recipe.Edges, Recipe.Cook)
+		if err != nil {
+			t.Error("expected and error")
+		}
+	})
+	t.Run("two requirements", func(t *testing.T) {
+		sleep := time.Second / 20
+		recipes := []Recipe{
+			{ID: 1, CookTime: sleep},
+			{ID: 2, CookTime: sleep, Ingredients: []int{1, 3}},
+			{ID: 3, CookTime: sleep},
+		}
+		ctx := context.Background()
+		err := topological.Tasks(ctx, recipes, Recipe.Identifier, Recipe.Edges, Recipe.Cook)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+	t.Run("context canceled", func(t *testing.T) {
+		recipes := []Recipe{
+			{ID: 1, CookTime: time.Minute},
+			{ID: 2, CookTime: time.Minute},
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second/20)
+		t.Cleanup(cancel)
+		err := topological.Tasks(ctx, recipes, Recipe.Identifier, Recipe.Edges, Recipe.Cook)
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Errorf("expected deadline exceeded error, got: %v", err)
+		}
+	})
+	t.Run("task failure", func(t *testing.T) {
+		recipes := []Recipe{
+			{ID: 1, CookTime: time.Minute, Ingredients: []int{2}},
+			{ID: 2, CookTime: time.Minute, IsBad: true},
+		}
+		ctx := context.Background()
+		err := topological.Tasks(ctx, recipes, Recipe.Identifier, Recipe.Edges, Recipe.Cook)
+		if !errors.Is(err, ErrBadRecipe) {
+			t.Errorf("expected bad recipe error, got: %v", err)
+		}
+	})
 }
