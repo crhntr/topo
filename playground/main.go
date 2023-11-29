6a10065ddff33088e9fe495ede4180242566c800
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2023-11-28 18:05:16 -0800

    add results and parameters

--- playground/main.go
+++ playground/main.go
@@ -3,36 +3,42 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
-	nodes := []node{
-		{InputIndexes: []int{}, Function: func() {
-			fmt.Println("init", 0)
-			<-time.After(time.Second)
-			fmt.Println("close", 0)
-		}},
-		{InputIndexes: []int{0}, Function: func() {
-			fmt.Println("init", 1)
-			<-time.After(time.Second)
-			fmt.Println("close", 1)
-		}},
-		{InputIndexes: []int{0}, Function: func() {
-			fmt.Println("init", 2)
-			<-time.After(time.Second)
-			fmt.Println("close", 2)
-		}},
-		{InputIndexes: []int{0}, Function: func() {
-			fmt.Println("init", 3)
-			<-time.After(time.Second)
-			fmt.Println("close", 3)
-		}},
+	nodes := []node[int]{
+		{InputIndexes: []int{}, Function: identity(2)},
+		{InputIndexes: []int{}, Function: identity(3)},
+		{InputIndexes: []int{0, 1}, Function: product},
+		{InputIndexes: []int{4, 0}, Function: product},
+		{InputIndexes: []int{0, 1, 2}, Function: sum},
 	}
-	run(nodes)
+	fmt.Println(run(nodes))
 }
 
-func waitingTask(node node, all []node) *sync.WaitGroup {
+func sum(in []int) int {
+	var s int
+	for _, i := range in {
+		s += i
+	}
+	return s
+}
+
+func product(in []int) int {
+	p := 1
+	for _, i := range in {
+		p *= i
+	}
+	return p
+}
+
+func identity(n int) func([]int) int {
+	return func([]int) int {
+		return n
+	}
+}
+
+func waitingTask[T any](node node[T], all []node[T]) *sync.WaitGroup {
 	wg := new(sync.WaitGroup)
 	for _, input := range node.InputIndexes {
 		wg.Add(1)
@@ -45,12 +51,13 @@ func waitingTask(node node, all []node) *sync.WaitGroup {
 	return wg
 }
 
-func run(nodes []node) {
+func run[T any](nodes []node[T]) []T {
 	for i := range nodes {
 		nodes[i].index = i
 		nodes[i].done = make(chan struct{})
 	}
 	for i := range nodes {
+		nodes[i].all = &nodes
 		nodes[i].inputs = waitingTask(nodes[i], nodes)
 	}
 	wg := sync.WaitGroup{}
@@ -62,19 +69,30 @@ func run(nodes []node) {
 		}(i)
 	}
 	wg.Wait()
+	results := make([]T, len(nodes))
+	for i := range nodes {
+		results[i] = nodes[i].Result
+	}
+	return results
 }
 
-type node struct {
+type node[T any] struct {
+	all          *[]node[T]
 	index        int
 	InputIndexes []int
-	Function     func()
+	Function     func(in []T) T
+	Result       T
 
 	done   chan struct{}
 	inputs *sync.WaitGroup
 }
 
-func (t *node) run() {
+func (t *node[T]) run() {
 	defer close(t.done)
 	t.inputs.Wait()
-	t.Function()
+	in := make([]T, len(t.InputIndexes))
+	for i, input := range t.InputIndexes {
+		in[i] = (*t.all)[input].Result
+	}
+	t.Result = t.Function(in)
 }
